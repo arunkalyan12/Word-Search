253f759d0efa5bd246ed455b0efd975bfb6c01b4
jbit22756 <145900442+jbit22756@users.noreply.github.com>
2024-03-22 23:40:45 +0530

    first commit

---
+++ wordSearchSolver.py
@@ -0,0 +1,41 @@
+def find_word(wordsearch, word):
+    for i in range(len(wordsearch)):
+        for j in range(len(wordsearch[i])):
+            if wordsearch[i][j] == word[0]:
+                if search_dir(wordsearch, word, (i, j)):
+                    print(f"Word '{word}' found at position ({i}, {j})")
+                    return
+    print(f"Word '{word}' not found in the wordsearch")
+
+def search_dir(wordsearch, word, start_pos):
+    directions = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
+
+    for dir in directions:
+        found = True
+        positions = [start_pos]
+
+        for i in range(1, len(word)):
+            x, y = positions[-1][0] + dir[0], positions[-1][1] + dir[1]
+
+            if 0 <= x < len(wordsearch) and 0 <= y < len(wordsearch[x]):
+                if wordsearch[x][y] == word[i]:
+                    positions.append((x, y))
+                else:
+                    found = False
+                    break
+            else:
+                found = False
+                break
+
+        if found:
+            for i in range(len(wordsearch)):
+                line = ""
+                for j in range(len(wordsearch[i])):
+                    if (i, j) in positions:
+                        line += " " + wordsearch[i][j]
+                    else:
+                        line += " -"
+                print(line)
+            return True
+
+    return False
